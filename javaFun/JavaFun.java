00c95a46b138c5f074fc400f6fcc1b355bc3bbf0
kimi01s <ksotelocalderon@gmail.com>
2023-04-26 22:24:58 -0400

    JavaFun

---
+++ javaFun/JavaFun.java
@@ -0,0 +1,7 @@
+public class JavaFun {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Kimberly Sotelo");
+        System.out.println("Tengo 25 años de edad.");
+        System.out.println("Mi ciudad es Santiago, Chile");
+    }
+}
